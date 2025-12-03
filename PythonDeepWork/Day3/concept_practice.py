04635c0b262c8ee7f1100f7ce4612caa3677cf74
SachinMate <sachin.mate@byjus.com>
2025-12-03 10:49:27 +0530

    Additional practice

--- PythonDeepWork/Day3/concept_practice.py
+++ PythonDeepWork/Day3/concept_practice.py
@@ -69,3 +69,63 @@ print(paload["lastname"])
 #This is EXACTLY how you build requests in API automation.
 
 #List and Dict operations
+#✔️List operations
+nums = [10,20,30,40,50]
+
+nums[1] #get item 20
+
+nums[2]=35 #Modify item
+
+nums.append(21) #Append
+
+nums.pop() #removes last
+nums.pop(1) #removes index 1
+
+# ✔️Dict operations
+user = {
+     "name":"sachin",
+     "l_name":"mate",
+     "role":"QA",
+     "exp":7
+ }
+
+print(user["name"]) #get value
+user["role"]="SDET" #Set value
+user["location"]="Pune" #Add new key
+
+#Check key exist
+if "name" in user:
+    print("Yes")
+
+#Iterate
+for key,value in user.items():
+    print(key, value)
+
+#Safe get
+print(user.get("exp",0)) #default 0
+
+#Mini practice example
+# a = input("Enter a value")
+# try:
+#     if a==int(a):
+#         print("Yes")
+# except ValueError:
+#     print("Error not a number")
+
+#Parse following json
+import json
+lapy = '{"id": 101, "product": "Laptop", "price": 55000}'
+data = json.loads(lapy)
+print(data["price"])
+
+#create dict form list
+names = ["sachin","subhash","mate"]
+dict = {}
+for name in names:
+    l = len(name)
+    dict[name]=l
+print(dict)
+
+#Dict comprehension
+len_dict ={ name: len(name) for name in names}
+print(len_dict)
