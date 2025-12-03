120f573baafad4b3ef3193c6f32c93156d1cbcc5
SachinMate <sachin.mate@byjus.com>
2025-12-03 10:05:19 +0530

    concept practice

---
+++ PythonDeepWork/Day3/concept_practice.py
@@ -0,0 +1,71 @@
+#Why do we use try/except? - to catch errors so the program doesn't crash
+
+#Simple example
+try:
+    x=10/0
+except ZeroDivisionError:
+    print("You cannot divide by zero")
+except ZeroDivisionError:
+    print('cannot dive by zero')
+
+#Example with multiple exempts
+try:
+    value = int("Sachin")
+except ValueError:
+    print("Cannot convert text to number")
+
+
+#Example with finally(always runs)
+try:
+    f = open("data.txt")
+except FileNotFoundError:
+    print("File not found")
+finally:
+    print("Done checking file")
+
+#Reading json(very common in API testing)
+#Suppose we have JSON text:
+import json
+json_str = '{"firstname":"aditi","mid_name":"sachin","l_name":"mate","skills":["AI","Python"],"age":30}'
+#Convert string to python dict
+data = json.loads(json_str)
+print(data["firstname"])
+print(data["mid_name"])
+print("l_name")
+print(data["age"])
+
+#Reading json from file
+import json
+with open("user.json") as f:
+    data = json.load(f)
+print(data["user"])
+print(data["role"])
+print(data)
+
+#Functions with return
+#Basic example
+def add(a,b):
+    return a+b
+result =  add(5,4)
+print(result)
+
+#Function returning multiple value
+def get_user():
+    return "sachin", 30, "QA engineer"
+name, age, role = get_user()
+print(name, age, role )
+
+#Function returning dict(common in API framework)
+def get_user_payload():
+    return {
+    "firstname":"sachin",
+    "lastname":"Mate",
+    "position": "QA Enginerr",
+    "totalPrice":25000
+    }
+paload = get_user_payload()
+print(paload["firstname"])
+print(paload["lastname"])
+#This is EXACTLY how you build requests in API automation.
+
+#List and Dict operations
